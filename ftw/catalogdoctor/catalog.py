a9eae33c0db6c6f95332ce327d4eb6a608a3971c
David Erni <david.erni@4teamwork.ch>
2019-04-16 15:25:58 +0200

    Split checkup result into its own class.

--- ftw/catalogdoctor/catalog.py
+++ ftw/catalogdoctor/catalog.py
@@ -19,35 +19,27 @@ class RidAberration(object):
         return set(self._catalog_symptoms.keys())
 
 
-class CatalogCheckup(object):
-    """Provide health checkup for a Products.ZCatalog.Catalog instance.
+class CheckupResult(object):
+    """Provide health result for one catalog checkup run."""
 
-    Validates that the catalogs uid and rid mapping and metadata is consistent.
-    This means that:
-    - the mappings have the same length
-    - the mappings are consistent, so every item is in the reverse mapping
-    - for every item there is also an entry in the catalog metadata
-
-    It does not validate indices and index data yet.
-    """
-
-    def __init__(self, catalog=None):
-        self.portal_catalog = catalog or api.portal.get_tool('portal_catalog')
-        self.catalog = self.portal_catalog._catalog
-
+    def __init__(self):
         self.aberrations = dict()
+        self._status = {
+            'uids': {
+                'length': None
+            },
+            'paths': {
+                'length': None
+            },
+            'data': {
+                'length': None
+            },
+        }
 
-    def is_healthy(self):
-        """Return whether the catalog is healthy according to this checkup."""
-
-        return not self.aberrations and self.is_length_healthy()
-
-    def is_length_healthy(self):
-        return (
-            len(self.catalog.paths)
-            == len(self.catalog.uids)
-            == len(self.catalog.data)
-        )
+    def report_catalog_stats(self, uids_length, paths_length, data_length):
+        self._status['uids']['length'] = uids_length
+        self._status['paths']['length'] = paths_length
+        self._status['data']['length'] = data_length
 
     def report_aberration(self, rid, path=None):
         """Report an aberration for a rid."""
@@ -64,49 +56,84 @@ class CatalogCheckup(object):
     def get_symptoms(self, rid):
         return self.aberrations[rid].catalog_symptoms
 
+    def is_healthy(self):
+        """Return whether the catalog is healthy according to this result."""
+
+        return not self.aberrations and self.is_length_healthy()
+
+    def is_length_healthy(self):
+        return (
+            self._status['uids']['length']
+            == self._status['paths']['length']
+            == self._status['data']['length']
+        )
+
+
+class CatalogCheckup(object):
+    """Run health checkup for a Products.ZCatalog.Catalog instance.
+
+    Validates that the catalogs uid and rid mapping and metadata is consistent.
+    This means that:
+    - the mappings have the same length
+    - the mappings are consistent, so every item is in the reverse mapping
+    - for every item there is also an entry in the catalog metadata
+
+    It does not validate indices and index data yet.
+    """
+
+    def __init__(self, catalog=None):
+        self.portal_catalog = catalog or api.portal.get_tool('portal_catalog')
+        self.catalog = self.portal_catalog._catalog
+
     def run(self):
+        result = CheckupResult()
+
         paths = self.catalog.paths
         paths_values = set(self.catalog.paths.values())
-
         uids = self.catalog.uids
         uids_values = set(self.catalog.uids.values())
+        data = self.catalog.data
+
+        result.report_catalog_stats(len(uids), len(paths), len(data))
 
         for path, rid in uids.items():
             if rid not in paths:
-                self.report_symptom(
+                result.report_symptom(
                     'in_uids_values_not_in_paths_keys', rid, path=path)
             elif paths[rid] != path:
-                self.report_symptom(
+                result.report_symptom(
                     'paths_tuple_mismatches_uids_tuple', rid, path=path)
 
             if path not in paths_values:
-                self.report_symptom(
+                result.report_symptom(
                     'in_uids_keys_not_in_paths_values', rid, path=path)
 
-            if rid not in self.catalog.data:
-                self.report_symptom(
+            if rid not in data:
+                result.report_symptom(
                     'in_uids_values_not_in_metadata_keys', rid, path=path)
 
         for rid, path in paths.items():
             if path not in uids:
-                self.report_symptom(
+                result.report_symptom(
                     'in_paths_values_not_in_uids_keys', rid, path=path)
             elif uids[path] != rid:
-                self.report_symptom(
+                result.report_symptom(
                     'uids_tuple_mismatches_paths_tuple', rid, path=path)
 
             if rid not in uids_values:
-                self.report_symptom(
+                result.report_symptom(
                     'in_paths_keys_not_in_uids_values', rid, path=path)
 
-            if rid not in self.catalog.data:
-                self.report_symptom(
+            if rid not in data:
+                result.report_symptom(
                     'in_paths_keys_not_in_metadata_keys', rid, path=path)
 
-        for rid in self.catalog.data:
+        for rid in data:
             if rid not in paths:
-                self.report_symptom(
+                result.report_symptom(
                     'in_metadata_keys_not_in_paths_keys', rid)
             if rid not in uids_values:
-                self.report_symptom(
+                result.report_symptom(
                     'in_metadata_keys_not_in_uids_values', rid)
+
+        return result
--- ftw/catalogdoctor/tests/test_catalog_checkup.py
+++ ftw/catalogdoctor/tests/test_catalog_checkup.py
@@ -15,108 +15,101 @@ class TestCatalogCheckup(FunctionalTestCase):
 
     def run_checkup(self):
         checkup = CatalogCheckup(self.portal_catalog)
-        checkup.run()
-        return checkup
+        return checkup.run()
 
     def test_initial_catalog_is_healthy(self):
-        checkup = self.run_checkup()
+        result = self.run_checkup()
 
-        self.assertTrue(checkup.is_healthy())
-        self.assertTrue(checkup.is_length_healthy())
+        self.assertTrue(result.is_healthy())
+        self.assertTrue(result.is_length_healthy())
 
     def test_aberrations_make_catalog_unhealthy(self):
-        checkup = self.run_checkup()
-        self.assertTrue(checkup.is_healthy())
+        result = self.run_checkup()
+        self.assertTrue(result.is_healthy())
 
-        checkup.report_aberration(self.choose_next_rid())
-        self.assertFalse(checkup.is_healthy())
+        result.report_aberration(self.choose_next_rid())
+        self.assertFalse(result.is_healthy())
 
     def test_longer_uids_make_catalog_unhealthy(self):
-        checkup = self.run_checkup()
-        self.assertTrue(checkup.is_length_healthy())
+        self.catalog.uids['foo'] = self.choose_next_rid()
 
-        checkup.catalog.uids['foo'] = self.choose_next_rid()
-
-        self.assertFalse(checkup.is_length_healthy())
+        result = self.run_checkup()
+        self.assertFalse(result.is_length_healthy())
 
     def test_longer_paths_make_catalog_unhealthy(self):
-        checkup = self.run_checkup()
-        self.assertTrue(checkup.is_length_healthy())
+        self.catalog.paths[self.choose_next_rid()] = 'foo'
 
-        checkup.catalog.paths[self.choose_next_rid()] = 'foo'
-
-        self.assertFalse(checkup.is_length_healthy())
+        result = self.run_checkup()
+        self.assertFalse(result.is_length_healthy())
 
     def test_longer_metadata_make_catalog_unhealthy(self):
-        checkup = self.run_checkup()
-        self.assertTrue(checkup.is_length_healthy())
+        self.catalog.data[self.choose_next_rid()] = dict()
 
-        checkup.catalog.data[self.choose_next_rid()] = dict()
-
-        self.assertFalse(checkup.is_length_healthy())
+        result = self.run_checkup()
+        self.assertFalse(result.is_length_healthy())
 
     def test_detects_duplicate_entry_in_rid_to_path_mapping_keys(self):
         broken_rid = self.choose_next_rid()
         self.catalog.paths[broken_rid] = get_physical_path(self.folder)
         self.catalog._length.change(1)
 
-        checkup = self.run_checkup()
+        result = self.run_checkup()
 
-        self.assertFalse(checkup.is_healthy())
-        self.assertEqual(1, len(checkup.aberrations))
+        self.assertFalse(result.is_healthy())
+        self.assertEqual(1, len(result.aberrations))
         self.assertEqual(
             {
                 'uids_tuple_mismatches_paths_tuple',
                 'in_paths_keys_not_in_metadata_keys',
                 'in_paths_keys_not_in_uids_values',
             },
-            checkup.get_symptoms(broken_rid))
+            result.get_symptoms(broken_rid))
 
     def test_detects_missing_entry_in_rid_to_path_mapping_values(self):
         path = get_physical_path(self.folder)
         rid = self.catalog.uids.pop(path)
 
-        checkup = self.run_checkup()
+        result = self.run_checkup()
 
-        self.assertFalse(checkup.is_healthy())
-        self.assertEqual(1, len(checkup.aberrations))
+        self.assertFalse(result.is_healthy())
+        self.assertEqual(1, len(result.aberrations))
         self.assertEqual(
             {
                 'in_metadata_keys_not_in_uids_values',
                 'in_paths_keys_not_in_uids_values',
                 'in_paths_values_not_in_uids_keys',
             },
-            checkup.get_symptoms(rid))
+            result.get_symptoms(rid))
 
     def test_detects_duplicate_entry_in_path_to_rid_mapping(self):
         path = get_physical_path(self.folder)
         rid = self.catalog.uids[path]
         self.catalog.uids['/some/other/path'] = rid
 
-        checkup = self.run_checkup()
+        result = self.run_checkup()
 
-        self.assertFalse(checkup.is_healthy())
-        self.assertEqual(1, len(checkup.aberrations))
+        self.assertFalse(result.is_healthy())
+        self.assertEqual(1, len(result.aberrations))
         self.assertEqual(
             {
                 'paths_tuple_mismatches_uids_tuple',
                 'in_uids_keys_not_in_paths_values',
             },
-            checkup.get_symptoms(rid))
+            result.get_symptoms(rid))
 
     def test_detects_missing_entry_in_path_to_rid_mapping(self):
         path = get_physical_path(self.folder)
         rid = self.catalog.uids[path]
         del self.catalog.paths[rid]
 
-        checkup = self.run_checkup()
+        result = self.run_checkup()
 
-        self.assertFalse(checkup.is_healthy())
-        self.assertEqual(1, len(checkup.aberrations))
+        self.assertFalse(result.is_healthy())
+        self.assertEqual(1, len(result.aberrations))
         self.assertEqual(
             {
                 'in_metadata_keys_not_in_paths_keys',
                 'in_uids_keys_not_in_paths_values',
                 'in_uids_values_not_in_paths_keys',
             },
-            checkup.get_symptoms(rid))
+            result.get_symptoms(rid))
