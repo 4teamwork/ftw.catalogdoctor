18008c9d1d5f00551fc12ab46ddaf323fc983de4
David Erni <david.erni@4teamwork.ch>
2019-06-19 12:34:07 +0200

    Add plone 5.1 classifier.

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     classifiers=[
         'Framework :: Plone',
         'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.1',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
