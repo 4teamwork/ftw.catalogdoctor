6e1e8b075bff23e8dfa1d1d84c2dedd6d1f45276
David Erni <d.erni@4teamwork.ch>
2020-09-24 18:06:26 +0200

    Capture index logging during testrun.

--- ftw/catalogdoctor/testing.py
+++ ftw/catalogdoctor/testing.py
@@ -2,12 +2,33 @@ from ftw.builder.content import register_dx_content_builders
 from ftw.builder.testing import BUILDER_LAYER
 from ftw.builder.testing import functional_session_factory
 from ftw.builder.testing import set_builder_session_factory
+from ftw.testing.layer import COMPONENT_REGISTRY_ISOLATION
 from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import PloneSandboxLayer
 from plone.testing import z2
 from zope.configuration import xmlconfig
-from ftw.testing.layer import COMPONENT_REGISTRY_ISOLATION
+import logging
+import sys
+
+
+# Capture messages logged by certain indexes, we want to see these messages
+# during test execution for better debugging/analysis.
+# The loggers used by different indexes are not standardized, unfortunately.
+LOG_TO_STDOUT_DURING_TESTRUN = {
+    'BooleanIndex.UnIndex': logging.ERROR,
+    'DateIndex': logging.ERROR,
+    'Products.ZCatalog': logging.ERROR,
+    'Zope.PathIndex': logging.DEBUG,
+    'Zope.UnIndex': logging.ERROR,
+}
+
+loghandler = logging.StreamHandler(stream=sys.stdout)
+loghandler.setLevel(logging.WARNING)
+for name, level in LOG_TO_STDOUT_DURING_TESTRUN.items():
+    logger = logging.getLogger(name)
+    logger.addHandler(loghandler)
+    logger.setLevel(level)
 
 
 class CatalogdoctorLayer(PloneSandboxLayer):
